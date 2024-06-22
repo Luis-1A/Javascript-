f7b125f819b16f22cccc7712b0e67a596644badc
Luis Fernando santos <142061648+Luis-1A@users.noreply.github.com>
2024-06-22 18:39:36 -0300

    hh

---
+++ js/dados.js
@@ -0,0 +1,261 @@
+var MENU = {
+
+    "burgers": [
+        {
+            "id": "1",
+            "img": "images/burguer.png",
+            "name": "Gramercy Tavern",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "2",
+            "img": "images/burguer.png",
+            "name": "Shake Shack",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "images/burguer.png",
+            "name": "Gott's Roadside",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 99
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img":  "images/burguer.png",
+            "name": "Emmy Squared",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 99
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "images/burguer.png",
+            "name": "Shake Shack",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 89
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img":  "images/burguer.png",
+            "name": "Peter Luger Steak House",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 175.95
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img":  "images/burguer.png",
+            "name": "Holeman & Finch",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 79
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img":  "images/burguer.png",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 109
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "images/burguer.png",
+            "name": "Raoul's",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img":  "images/burguer.png",
+            "name": "Flannery Beef",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 79
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img":  "images/burguer.png",
+            "name": "Gott's Roadside",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 149
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img":  "images/burguer.png",
+            "name": "Gramercy Tavern",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 149
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
+            "name": "New Territories",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/bebidas/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg",
+            "name": "Guy Fieri",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 69
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 39
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cardapio/bebidas/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg",
+            "name": "Franco's Hurricane Mix",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 39
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/cardapio/bebidas/margarita-mix.bd48a000d589d3147b14790af3c33fcd.jpg",
+            "name": "Johnny Sanchez",
+            "dsc": "Margarita Mix",
+            "price": 59
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup-2-pack.0ac76063f151988113cbaabd0eaa829f.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 59
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cardapio/bebidas/unicorn-parade-milkshake-kit-for-2.9052d04c1cf25b29442048bd3e535f21.jpg",
+            "name": "New Territories",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 109
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cardapio/bebidas/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
+            "name": "The Hummus & Pita Co.",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 89
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cardapio/bebidas/old-honey-barn-mint-julep-mixer-200ml.e0b131d6d9b69963706b43fd4334ab74.jpg",
+            "name": "Old Honey Barn Mint Julep",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 25
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cardapio/bebidas/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 59.95
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cardapio/bebidas/weekender-charleston-bloody-mary-mix-bold-and-spicy.c372868c9937e407a299a22001e210e2.jpg",
+            "name": "Charleston Beverage Company",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 39.95
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/bebidas/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
+            "name": "Grady's Cold Brew",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 40
+        }
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/pizzas.png", 
+            "name": "Lou Malnati's Pizza",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 67.99
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+"img": "images/pizzas.png", 
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+"img": "images/pizzas.png", 
+            "name": "Zuppardi's Apizza",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 79
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+"img": "images/pizzas.png", 
+            "name": "Lou Malnati's Pizza",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 116.99
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+"img": "images/pizzas.png", 
+            "name": "Pizzeria Bianco",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+"img": "images/pizzas.png", 
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+"img": "images/pizzas.png", 
+            "name": "Emmy Squared",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 89
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+"img": "images/pizzas.png", 
+            "name": "Paesan's Pizza",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 69
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+"img": "images/pizzas.png", 
+            "name": "My Pi Pizza",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+"img": "images/pizzas.png", 
+            "name": "Lou Malnati's Pizza",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 96.99
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+"img": "images/pizzas.png", 
+            "name": "Tony's Pizza Napoletana",
+            "dsc": "Choose Your Own Pizza - 3 Pack",
+            "price": 99
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+"img": "images/pizzas.png", 
+            "name": "The Columbia Inn",
+            "dsc": "Plain Thin Crust Pizza - 4 Pack",
+            "price": 79
+        }
+    ]
+}
